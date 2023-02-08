9f7e91ed47c0988b75090fc8243cad7dd679c9a6
jasonli8408 <jasonli8408@gmail.com>
2023-02-08 16:38:57 -0500

    update disabled booking js

--- booking.js
+++ booking.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
+    $("input[id=btnradio1]").prop("disabled", true);
     $('input[name=btnradio]').click(function() {
         var target = $(this).data('target');
         $('.collapse').collapse('hide');
+        $(this).prop("disabled", true);
+        $("input[type='radio']").not(this).prop("disabled", false);
         $(target).collapse('show');
     });
 });

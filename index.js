aa47eecee9d87f9bd729970fece4eeede0f8084c
Jenci <jenci2114@gmail.com>
2023-02-09 22:38:57 -0500

    Added a hard-coded filter

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ function apply_filter() {
 function reset_filter() {
     $(".filter-hide").show()
     $(".filter-hide-2").show()
+    $("#startDate").val('')
+    $("#endDate").val('')
+    $("#guests").val('')
 }
 
 function apply_filter_two() {
     reset_filter()
     $("#guests").val('3')
     $("#order").val('name_asc')
+    $("#startDate").val('2023-02-06')
+    $("#endDate").val('2023-02-07')
     $(".filter-hide-2").hide()
 }

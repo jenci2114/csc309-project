edd75ffa3107db9958c18ac8df6fb0a611d504ac
Jenci <jenci2114@gmail.com>
2023-02-09 22:37:20 -0500

    Added a hard-coded filter

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 function apply_filter() {
+    reset_filter()
+    $("#guests").val('2')
+    $("#order").val('rating_desc')
     $(".filter-hide").hide()
 }
 
 function reset_filter() {
     $(".filter-hide").show()
+    $(".filter-hide-2").show()
+}
+
+function apply_filter_two() {
+    reset_filter()
+    $("#guests").val('3')
+    $("#order").val('name_asc')
+    $(".filter-hide-2").hide()
 }

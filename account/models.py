c254c9c20a6d25e6125cd5e50f9b05d5cc3f53d5
Yupeng Chang <changyp01@gmail.com>
2023-03-08 15:21:25 -0500

    some issue with use models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, Group
+
 
 # Create your models here.
+class User(AbstractUser):
+    # username = models.CharField(max_length=150, unique=True)
+    # email = models.EmailField(max_length=254)
+    # first_name = models.CharField(max_length=30)
+    # last_name = models.CharField(max_length=30)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=0.00)
+    phone = models.CharField(max_length=15)
+    location = models.CharField(max_length=100)
+    #
+    # groups = models.ManyToManyField(
+    #     Group,
+    #     verbose_name='groups',
+    #     blank=True,
+    #     help_text='The groups this user belongs to. A user will '
+    #               'get all permissions granted to each of their groups.',
+    #     related_name='custom_user_set',  # add a related_name argument
+    #     related_query_name='custom_user',
+    # )
--- property/models.py
+++ property/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from account.models import User
 
-# Create your models here.
+
+class Property(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=100)
+    province = models.CharField(max_length=100)
+    zip = models.CharField(max_length=10)
+    beds = models.IntegerField()
+    bathrooms = models.IntegerField()
+    max_guests = models.IntegerField()
+    description = models.TextField()
+    image_url = models.URLField()
+
+
+class PropertyAvailability(models.Model):
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price_per_night = models.DecimalField(max_digits=8, decimal_places=2)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+
+
+class Reservation(models.Model):
+    host = models.ForeignKey(User, on_delete=models.CASCADE)
+    client = models.ForeignKey(User, on_delete=models.CASCADE)
+    property = models.ForeignKey(Property, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=50, default='pending')
+    start_date = models.DateField()
+    end_date = models.DateField()
+    host_to_user_msg = models.TextField(blank=True)
+    host_to_user_rating = models.IntegerField(null=True, blank=True)
+    user_to_property_rating = models.IntegerField(null=True, blank=True)
+    number_of_property_comment = models.IntegerField(default=0)
+
+
+class PropertyComment(models.Model):
+    msg = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reservation = models.ForeignKey(Reservation, on_delete=models.CASCADE)
+    comment_number = models.IntegerField()
+
+
+class Notification(models.Model):
+    msg = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+    read = models.BooleanField(default=False)
+    is_host = models.BooleanField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)

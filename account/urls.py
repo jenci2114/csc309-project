2206031eaffc18b74f20f1aac030f1ab913a4242
jasonli8408 <jasonli8408@gmail.com>
2023-03-08 15:53:12 -0500

    add serializer and token verify

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from .views import *
 from django.urls import path
 
@@ -6,5 +6,6 @@ app_name = 'api'
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     #path('token/blacklist/')
 ]

1e1bc9476a6f2e0d08b57e8aafb9012e8b25f60f
jasonli8408 <jasonli8408@gmail.com>
2023-03-08 16:05:26 -0500

    register models to admin

--- property/admin.py
+++ property/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Property, propertyImage, PropertyAvailability, Reservation, PropertyComment
+# Register your models here.
+admin.site.register(Property)
+admin.site.register(propertyImage)
+admin.site.register(PropertyAvailability)
+admin.site.register(Reservation)
+admin.site.register(PropertyComment)
+

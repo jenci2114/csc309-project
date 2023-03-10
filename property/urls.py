1abe6c545bb935a91fb254825ae0fc39b01f127b
Jenci <jenci2114@gmail.com>
2023-03-10 16:29:20 -0500

    Property update availability

--- property/urls.py
+++ property/urls.py
@@ -3,6 +3,7 @@ from property.views.create_availability import CreateAvailabilityView
 from property.views.create_property import CreatePropertyView
 from property.views.delete_property import DeletePropertyView
 from property.views.list_property import ListPropertyView
+from property.views.update_availabiity import UpdateAvailabilityView
 from property.views.update_property import UpdatePropertyView
 
 app_name = 'property'
@@ -12,4 +13,5 @@ urlpatterns = [
     path('search/', ListPropertyView.as_view(), name='search'),
     path('delete/<int:pk>/', DeletePropertyView.as_view(), name='delete'),
     path('availability/create/<int:pk>/', CreateAvailabilityView.as_view(), name='create_availability'),
+    path('availability/update/<int:pk>/', UpdateAvailabilityView.as_view(), name='update_availability'),
 ]
--- property/views/update_availabiity.py
+++ property/views/update_availabiity.py
@@ -1,36 +1,43 @@
 from rest_framework.exceptions import PermissionDenied, ValidationError
-from rest_framework.generics import CreateAPIView, get_object_or_404
+from rest_framework.generics import get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
-from property.models import Property, PropertyAvailability
+from property.models import PropertyAvailability
 from property.serializers import AvailabilitySerializer
 
 
-class CreateAvailabilityView(CreateAPIView):
+class UpdateAvailabilityView(UpdateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = AvailabilitySerializer
 
     def get_object(self):
-        return get_object_or_404(Property, pk=self.kwargs['pk'])
+        return get_object_or_404(PropertyAvailability, pk=self.kwargs['pk'])
 
-    def perform_create(self, serializer):
-        if self.request.user == self.get_object().user:
+    def perform_update(self, serializer):
+        if 'start_date' not in serializer.validated_data:
+            serializer.validated_data['start_date'] = self.get_object().start_date
+        if 'end_date' not in serializer.validated_data:
+            serializer.validated_data['end_date'] = self.get_object().end_date
+
+        if self.request.user == self.get_object().property.user:
             if serializer.validated_data['start_date'] > serializer.validated_data['end_date']:
                 raise ValidationError('Start date must be before end date.')
 
             conflicts = PropertyAvailability.objects.filter(
                 start_date__lte=serializer.validated_data['end_date'],
                 start_date__gte=serializer.validated_data['start_date'],
-                property_id=self.get_object().id
+                property_id=self.get_object().property.id
             ) | PropertyAvailability.objects.filter(
                 end_date__lte=serializer.validated_data['end_date'],
                 end_date__gte=serializer.validated_data['start_date'],
-                property_id=self.get_object().id
+                property_id=self.get_object().property.id
             )
+
+            conflicts = conflicts.exclude(pk=self.get_object().id)
 
             if conflicts.exists():
                 raise ValidationError('This availability conflicts with another availability.')
 
-            serializer.save(property=self.get_object())
+            serializer.save()
         else:
-            raise PermissionDenied('You do not have permission to create this availability.')
+            raise PermissionDenied('You do not have permission to edit this availability.')

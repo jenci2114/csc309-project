0d5354d0ed5cb0607c8d68eb59b108d8e034476a
Jenci <jenci2114@gmail.com>
2023-03-10 10:47:36 -0500

    Update property only allows owner to update

--- property/views/update_property.py
+++ property/views/update_property.py
@@ -1,3 +1,4 @@
+from rest_framework.exceptions import PermissionDenied
 from rest_framework.generics import get_object_or_404, UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
@@ -12,3 +13,9 @@ class UpdatePropertyView(UpdateAPIView):
 
     def get_object(self):
         return get_object_or_404(Property, pk=self.kwargs['pk'])
+
+    def perform_update(self, serializer):
+        if self.request.user == self.get_object().user:
+            serializer.save()
+        else:
+            raise PermissionDenied('You do not have permission to edit this property.')

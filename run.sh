cf25a143f206650b96889942cc0c54a4f8a49c21
jasonli8408 <jasonli8408@gmail.com>
2023-03-11 16:57:29 -0500

    shell script simplify

--- run.sh
+++ run.sh
@@ -1,18 +1,2 @@
-#!/bin/bash
 
-if [[ "$(uname)" == "Linux" ]]; then
-    # Ubuntu
-    ./manage.py makemigrations
-    ./manage.py migrate
-    ./manage.py loaddata data.json
-    ./manage.py runserver
-elif [[ "$(uname)" == "Darwin" ]]; then
-    # Mac OS
-    python3 ./manage.py makemigrations  
-    python3 ./manage.py migrate
-    python3 ./manage.py loaddata data.json
-    python3 ./manage.py runserver
-else 
-    echo "OS not supported"
-    exit 1
-fi
+python3 ./manage.py runserver

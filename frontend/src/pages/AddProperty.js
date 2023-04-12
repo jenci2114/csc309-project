063a3b2383a8b94321e776471d0bd074d62e5868
jasonli8408 <jasonli8408@gmail.com>
2023-04-12 00:16:37 -0400

    clear when submit and pop up message

--- frontend/src/pages/AddProperty.js
+++ frontend/src/pages/AddProperty.js
@@ -15,6 +15,7 @@ export default function AddProperty() {
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
     const [property_id, setPropertyId] = useState(0);
+    const [added, setAdded] = useState(false);
 
 
     if (!isLoggedin) {
@@ -44,7 +45,7 @@ export default function AddProperty() {
             })
             .catch(error => console.error(error));
 
-        
+        console.log(property_id);
         if (property_id !== 0) {
             const formData = new FormData();
             formData.append("image", imageFile);
@@ -59,25 +60,37 @@ export default function AddProperty() {
                 .then(data => console.log(data))
                 .catch(error => console.error(error));
         }   
+        setAddress("");
+        setCity("");
+        setProvince("");
+        setZip("");
+        setBeds(0);
+        setBaths(0);
+        setGuests(0);
+        setDescription("");
+        setImage("");
+        setAdded(true);
     };
 
     return (
         <>
         <div className="container">
+
         <h1 className="fs-1 fw-semibold text-center">Add property</h1>
+        {added && <div className="alert alert-success" style={{ paddingBottom: "0px" }} role="alert"><p className="text-center">Successfully Added Property</p></div>}
 
         <div className="row g-3">
             <div className="col-12">
               <label htmlFor="inputAddress" className="form-label">Address</label>
-              <input type="text" className="form-control" id="inputAddress" placeholder="Apartment, studio, or floor" required onChange={(event) => setAddress(event.target.value)}></input>
+              <input type="text" className="form-control" id="inputAddress" placeholder="Apartment, studio, or floor" required onChange={(event) => setAddress(event.target.value)} value={address}></input>
             </div>
             <div className="col-md-6">
               <label htmlFor="inputCity" className="form-label">City</label>
-              <input type="text" className="form-control" id="inputCity" required onChange={(event) => setCity(event.target.value)}></input>
+              <input type="text" className="form-control" id="inputCity" required onChange={(event) => setCity(event.target.value)} value={city}></input>
             </div>
             <div className="col-md-4">
               <label htmlFor="inputState" className="form-label">State/Province</label>
-              <select id="inputState" className="form-select" required defaultValue="" onChange={(event) => setProvince(event.target.value)}>
+              <select id="inputState" className="form-select" required onChange={(event) => setProvince(event.target.value)} value={province}>
                 <option value="">Choose...</option>
                 <option value="British Columbia">British Columbia</option>
                 <option value="California">California</option>
@@ -87,27 +100,27 @@ export default function AddProperty() {
             </div>
             <div className="col-md-2">
               <label htmlFor="inputZip" className="form-label">Zip</label>
-              <input type="text" className="form-control" id="inputZip" required onChange={(event) => setZip(event.target.value)}></input>
+              <input type="text" className="form-control" id="inputZip" required onChange={(event) => setZip(event.target.value)} value={zip}></input>
             </div>
             <div className="col-md-4">
                 <label htmlFor="num_guests" className="form-label">Max # of Guests</label>
-                <input type="number" className="form-control" id="num_guests" min="0" max="100" required defaultValue="0" onChange={(event) => setGuests(event.target.value)}></input>
+                <input type="number" className="form-control" id="num_guests" min="0" max="100" required onChange={(event) => setGuests(event.target.value)} value={guests}></input>
               </div>
               <div className="col-md-4">
                 <label htmlFor="num_beds" className="form-label"># of Beds</label>
-                <input type="number" className="form-control" id="num_beds" min="0" max="100" required defaultValue="0" onChange={(event) => setBeds(event.target.value)}></input>
+                <input type="number" className="form-control" id="num_beds" min="0" max="100" required onChange={(event) => setBeds(event.target.value)} value={beds}></input>
               </div>
               <div className="col-md-4">
                 <label htmlFor="num_baths" className="form-label"># of Bathrooms</label>
-                <input type="number" className="form-control" id="num_baths" min="0" max="100" required defaultValue="0" onChange={(event) => setBaths(event.target.value)}></input>
+                <input type="number" className="form-control" id="num_baths" min="0" max="100" required  onChange={(event) => setBaths(event.target.value)} value={baths}></input>
               </div>
               <div className="col-md-12">
                 <label htmlFor="description" className="form-label">Description</label>
-                <textarea rows="3" className="form-control" id="description" required onChange={(event) => setDescription(event.target.value)}></textarea>
+                <textarea rows="3" className="form-control" id="description" required onChange={(event) => setDescription(event.target.value)} value={description}></textarea>
               </div>
               <div className="col-md-12">
                 <label htmlFor="image" className="form-label">Image</label>
-                <input type="file" className="form-control" id="image" accept="image/*" required onChange={(event) => setImage(event.target.value)}></input>
+                <input type="file" className="form-control" id="image" accept="image/*" required onChange={(event) => setImage(event.target.value)} value={image}></input>
               </div>
             <div className="col-12">
               <button type="submit" className="btn btn-primary" onClick={addProperty}>Add property</button>
